ab3a3e003c86ac35b2d05366fab540173ac5d4f1
RaShunda Lanier <developer@rashundawilliams.com>
2024-03-16 23:33:44 -0500

    initial commit
    go basics

---
+++ src/calc/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 13
+	y := 31
+
+	// Addition
+	add := x + y
+	fmt.Println("Result of ", x, " + ", y, " =", add)
+
+	// Subtraction
+	sub := add - y
+	fmt.Println("Result of ", add, " - ", y, " =", sub)
+
+}
