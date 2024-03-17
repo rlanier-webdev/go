bfac6d39600ed43350eea9cc44597cc330183340
RaShunda Lanier <developer@rashundawilliams.com>
2024-03-17 16:00:46 -0500

    added variable

--- src/comic/comic.go
+++ src/comic/comic.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var publisher, writer, artist, title string
+	var publisher, writer, artist, title, genre string
 	var year, pageNumber uint
 	var grade float32
 
@@ -15,8 +15,9 @@ func main() {
 	year = 1997
 	pageNumber = 14
 	grade = 6.5
+	genre = "Horror"
 
-	fmt.Println(title, "\nWritten by: ", writer, "\nArtist: ", artist, "\nPublisher: ", publisher, "\nYear: ", year, "\n# of Pages: ", pageNumber, "\nGrade: ", grade)
+	fmt.Println(title, "\nWritten by: ", writer, "\nArtist: ", artist, "\nPublisher: ", publisher, "\nYear: ", year, "\n# of Pages: ", pageNumber, "\nGrade: ", grade, "\nGenre: ", genre)
 
 	// Book 2
 	title = "Epic Vol. 1"
@@ -26,9 +27,26 @@ func main() {
 	year = 2013
 	pageNumber = 160
 	grade = 9.0
+	genre = "Crime"
 
 	fmt.Println("")
 	fmt.Println("")
 
-	fmt.Println(title, "\nWritten by: ", writer, "\nArtist: ", artist, "\nPublisher: ", publisher, "\nYear: ", year, "\n# of Pages: ", pageNumber, "\nGrade: ", grade)
+	fmt.Println(title, "\nWritten by: ", writer, "\nArtist: ", artist, "\nPublisher: ", publisher, "\nYear: ", year, "\n# of Pages: ", pageNumber, "\nGrade: ", grade, "\nGenre: ", genre)
+
+	// Book 3
+	title = "Dolly Vol. 1"
+	writer = "Ray N. Shawn"
+	artist = "Joy Nails"
+	publisher = "Ckode Publishing Inc."
+	year = 2024
+	pageNumber = 18
+	grade = 9.0
+	genre = "Crime"
+	ageRating := "T"
+
+	fmt.Println("")
+	fmt.Println("")
+
+	fmt.Println(title, "\nWritten by: ", writer, "\nArtist: ", artist, "\nPublisher: ", publisher, "\nYear: ", year, "\n# of Pages: ", pageNumber, "\nGrade: ", grade, "\nGenre: ", genre, "\nAge Rating: ", ageRating)
 }
