b774147dff1ceb0381cc869301f10c090cc4265f
RaShunda Lanier <developer@rashundawilliams.com>
2024-03-20 19:36:09 -0500

    Revert "fix switch formating"
    This reverts commit 462cc3be3e4bf8643dab754e947c6c35cc367784.

--- src/interstellartravel/main.go
+++ src/interstellartravel/main.go
@@ -16,15 +16,15 @@ func calculateFuel(planet string) int {
 	var fuel int
 
 	switch planet {
-		case "Venus":
-			fuel = 300000
-		case "Mercury":
-			fuel = 500000
-		case "Mars":
-			fuel = 700000
-		default:
-			fuel = 0
-		}
+	case "Venus":
+		fuel = 300000
+	case "Mercury":
+		fuel = 500000
+	case "Mars":
+		fuel = 700000
+	default:
+		fuel = 0
+	}
 
 	return fuel
 }
