a05fe76a443ac82c837c36b6c6e65e0550a6a9d3
RaShunda Lanier <developer@rashundawilliams.com>
2024-03-21 19:12:49 -0500

    Deprecation: Removed deprecated code usage
    - Removed import for 'time' package and seeding of the random number generator due to deprecation of the Seed method.

--- src/bankheist/bankheist.go
+++ src/bankheist/bankheist.go
@@ -3,14 +3,11 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"time"
 )
 
 func main() {
 	isHeistOn := true
 	eludedGuards := rand.Intn(100)
-
-	rand.Seed(time.Now().UnixNano())
 
 	if eludedGuards >= 50 {
 		fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
