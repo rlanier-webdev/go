619192abce995137bba7cded29e139f8e0b48a5e
RaShunda Lanier <developer@rashundawilliams.com>
2024-03-28 19:21:52 -0500

    update wiki
    added net/http package to serve pages
    added handlers for view and save pages
    added html/template package
    created template files, modified handlers, created rendertemplate function

---
+++ src/gowiki/wiki.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte //byte slice
+}
+
+// This is a method named save that takes as its receiver p, a pointer to Page . It takes no parameters, and returns a value of type error.
+func (p *Page) save() error {
+	filename := p.Title + ".txt" // save the Page's Body to a text file.
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+// allow users to VIEW a wiki page
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
+// allow users to EDIT a wiki page
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit", p)
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
