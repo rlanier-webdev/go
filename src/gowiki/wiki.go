d81cff997a9fd7c024d075e995421a316846766c
RaShunda Lanier <developer@rashundawilliams.com>
2024-03-30 17:58:07 -0500

    feat: added a homepage to list all wiki pages

--- src/gowiki/wiki.go
+++ src/gowiki/wiki.go
@@ -1,40 +1,58 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"regexp"
+	"path/filepath"
+	"strings"
+	
 )
+
 
 type Page struct {
 	Title string
 	Body  []byte //byte slice
 }
 
+var pageDir = "./pages/"
+
 // This is a method named save that takes as its receiver p, a pointer to Page . It takes no parameters, and returns a value of type error.
 func (p *Page) save() error {
-	filename := p.Title + ".txt" // save the Page's Body to a text file.
-	return os.WriteFile(filename, p.Body, 0600)
+	filename := pageDir + p.Title + ".txt" // save the Page's Body to a text file.
+	err := os.WriteFile(filename, p.Body, 0600)
+	if err != nil {
+		log.Printf("Error saving page %s: %v\n", p.Title, err)
+		return err
+	}
+	return nil
 }
 
 // load the page
 func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
-	body, err := os.ReadFile(filename)
+	
+	body, err := os.ReadFile(pageDir+filename)
+
 	if err != nil {
+		log.Printf("Error reading file %s: %v\n", filename, err)
+
 		return nil, err
 	}
+
 	return &Page{Title: title, Body: body}, nil
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("edit.html", "view.html", "home.html"))
 
 // render html files
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	err := templates.ExecuteTemplate(w, tmpl+".html", p)
+func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", data)
 	if err != nil {
+		log.Printf("Error rendering template %s: %v\n", tmpl, err)
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -60,6 +78,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	// if the requested Page doesn't exist, it should redirect the client to the edit Page so the content may be created
 	if err != nil {
+		log.Printf("Error handling view request for %s: %v\n", title, err)
 		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
 		return
 	}
@@ -90,11 +109,50 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+type Homepage struct {
+	Title string
+	Pages []*Page // Slice of pointers to Page structs
+}
+
+func getAvailablePageTitles() ([]*Page, error) {
+	var pages []*Page
+
+	files, err := os.ReadDir(pageDir) 
+	if err != nil {
+		log.Printf("Error reading directory %s: %v\n", pageDir, err)
+		return nil, err
+	}
+
+	for _, file := range files {
+		base := filepath.Base(file.Name())
+		title := strings.TrimSuffix(base, ".txt")
+		p, err := loadPage(title)
+		if err != nil {
+			return nil, err // Handle error loading individual pages
+		}
+		pages = append(pages, p)
+	}
+	return pages, nil
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	pages, err := getAvailablePageTitles()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	homepage := &Homepage{Title: "Homepage", Pages: pages}
+	renderTemplate(w, "home", homepage)
+}
+
 func main() {
 	// handlers
+	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
+	fmt.Println("Server listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
