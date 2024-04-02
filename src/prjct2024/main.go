740d7eeac7dd1ac3578d4c42219083e3eb027e29
RaShunda Lanier <developer@rashundawilliams.com>
2024-04-01 23:17:06 -0500

    setup project files

--- src/prjct2024/main.go
+++ src/prjct2024/main.go
@@ -1,16 +1,17 @@
 package main
 
-// import (
-// 	"fmt"
-// 	"html/template"
-// 	"log"
-// 	"net/http"
-// 	"os"
-// 	"path/filepath"
-// 	"regexp"
-// 	"strings"
-// )
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	fs := http.FileServer(http.Dir("./tmpl"))
+	http.Handle("/", fs)
 
+	log.Print("Listening on :3000...")
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
