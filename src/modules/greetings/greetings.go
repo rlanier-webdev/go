b3d4c40791fc8b663393d522b69ae713a1874bbc
RaShunda Lanier <developer@rashundawilliams.com>
2024-04-14 22:43:14 -0500

    update hello.go
    Create a names variable as a slice type holding three names.
    Pass the names variable as the argument to the Hellos function.

--- src/modules/greetings/greetings.go
+++ src/modules/greetings/greetings.go
@@ -22,9 +22,9 @@ func Hello(name string) (string, error) {
 func Hellos(names []string) (map[string]string, error) {
 	// A map to associate names with messages.
 	messages := make(map[string]string)
-	
+
 	// Loop through the received slice of names, calling the Hello function to get a message for each name.
-	for _,name := range names {
+	for _, name := range names {
 		message, err := Hello(name)
 		if err != nil {
 			return nil, err
@@ -39,7 +39,7 @@ func Hellos(names []string) (map[string]string, error) {
 func randomFormat() string {
 	// A slice of message formats.
 	formats := []string{
-		"Hi, %v. Welcome",
+		"Hi, %v. Welcome.",
 		"Great to see you, %v!",
 		"Hail, %v! Well met!",
 	}
